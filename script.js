0696765a0b44f9f01d933fe77fecda17c3853c45
Eric Huffman <ericdhuffman@gmail.com>
2025-01-31 15:46:59 -0500

    fixed hero button scroll action

--- script.js
+++ script.js
@@ -4,3 +4,8 @@ const nav = document.querySelector(".nav");
 hamburgerMenu.addEventListener("click", () => {
     nav.classList.toggle("active")
 });
+
+document.getElementById('hero-button').addEventListener('click', function(event) {
+    event.preventDefault();
+    document.getElementById('about').scrollIntoView({ behavior: 'smooth' });
+});

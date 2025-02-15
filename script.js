eaf79c7f08955056b46512a046cf5c527905fb1d
Eric Huffman <ericdhuffman@gmail.com>
2025-02-15 17:26:31 -0500

    fixing issues with responsiveness

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
+const header = document.querySelector('.header');
 const hamburgerMenu = document.querySelector(".hamburger-menu");
 const nav = document.querySelector(".nav");
+
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 50) {
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+    }
+});
 
 hamburgerMenu.addEventListener("click", () => {
     nav.classList.toggle("active")
